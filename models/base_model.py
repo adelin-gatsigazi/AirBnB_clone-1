473bea34c9c0613ecd3b40a477ea24741e7c2b88
kenza1525 <kerianenzabampema@gmail.com>
2023-03-07 06:23:30 -0800

    Added kwargs code in the init function

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,19 @@ import models
 class BaseModel:
     """ Defines all common attributes/methods for other classes """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initializes object attributes """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """ Returns a string representation of the class """
