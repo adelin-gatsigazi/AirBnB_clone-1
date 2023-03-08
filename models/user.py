89a090d5be3d0364fcb6ab34afdfcb12454f2713
kenza1525 <kerianenzabampema@gmail.com>
2023-03-08 06:49:10 -0800

    test

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" class User that inherits from BaseModel """
+
+from .base_model import BaseModel
+
+
+class User(BaseModel):
+    """ Defines the attributes of a user """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
