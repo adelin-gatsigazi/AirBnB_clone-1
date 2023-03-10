0d12f7e1ed99e5d8d07bd3c1b7211674fa134080
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2023-03-10 05:31:39 +0200

    Console update

--- console.py
+++ console.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
 """Defines the HBnB console."""
 import cmd
-from shlex import split
+import shlex 
 from models.base_model import BaseModel
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
     """Defines the BnB command interpreter"""
 
     prompt = "(hbnb) "
-
+    __classes = {
+        "BaseModel" :  BaseModel,
+        "Place" : Place,
+        "State" : State,
+        "City" : City,
+        "Amenity" : Amenity,
+        "Review" : Review
+    }
+    __file_path = "file.json"
+    __storage = FileStorage()
     
     def do_quit(self, arg):
         """command  exit the program."""
